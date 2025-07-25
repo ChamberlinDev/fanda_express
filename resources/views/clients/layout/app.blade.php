754e436644a28b4120835f7842fcda1c286ff214
ChamberlinDev <himalayaommet@gmail.com>
2025-07-25 05:38:43 +0000

    second commit

---
+++ resources/views/clients/layout/app.blade.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>The River</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="The River template project">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles/bootstrap-4.1.2/bootstrap.min.css">
+	<link href="plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/owl.theme.default.css">
+	<link rel="stylesheet" type="text/css" href="plugins/OwlCarousel2-2.3.4/animate.css">
+	<link href="plugins/jquery-datepicker/jquery-ui.css" rel="stylesheet" type="text/css">
+	<link href="plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="styles/main_styles.css">
+	<link rel="stylesheet" type="text/css" href="styles/responsive.css">
+</head>
+
+<body>
+
+	<div class="super_container">
+
+		<!-- Header -->
+
+		@include('clients.partials.header')
+
+		<!-- Menu -->
+
+		<!-- <div class="menu trans_400 d-flex flex-column align-items-end justify-content-start">
+		<div class="menu_close"><i class="fa fa-times" aria-hidden="true"></i></div>
+		<div class="menu_content">
+			<nav class="menu_nav text-right">
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="about.html">About us</a></li>
+					<li><a href="#">Rooms</a></li>
+					<li><a href="blog.html">Blog</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="menu_extra">
+			<div class="menu_book text-right"><a href="#">Book online</a></div>
+			<div class="menu_phone d-flex flex-row align-items-center justify-content-center">
+				<img src="images/phone-2.png" alt="">
+				<span>0183-12345678</span>
+			</div>
+		</div>
+	</div> -->
+
+		<!-- Home -->
+
+		<div class="home">
+			<div class="home_slider_container">
+				<div class="owl-carousel owl-theme home_slider">
+
+					<!-- Slide -->
+					<div class="slide">
+						<div class="background_image" style="background-image:url(images/index_1.jpg)"></div>
+						<div class="home_container">
+							<div class="container">
+								<div class="row">
+									<div class="col">
+										<div class="home_content text-center">
+											<div class="home_title">Bienvenu(e) sur Fanda-express</div>
+											<div class="booking_form_container">
+												<p class="text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi eaque cupiditate quo aspernatur quos inventore. Architecto, nobis dolore tenetur accusamus officiis, maxime, dolorum temporibus alias quia quidem eveniet nam quam!</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<!-- Slide -->
+					<div class="slide">
+						<div class="background_image" style="background-image:url(images/index_1.jpg)"></div>
+						<div class="home_container">
+							<div class="container">
+								<div class="row">
+									<div class="col">
+										<div class="home_content text-center">
+											<div class="home_title">Retrouvez votre luxe</div>
+											<div class="booking_form_container">
+												<p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe odio adipisci est atque in obcaecati reiciendis aspernatur. Dolore exercitationem maiores reprehenderit. Nam sint provident molestiae minima delectus? Optio, perferendis laboriosam.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<!-- Slide -->
+					<div class="slide">
+						<div class="background_image" style="background-image:url(images/index_1.jpg)"></div>
+						<div class="home_container">
+							<div class="container">
+								<div class="row">
+									<div class="col">
+										<div class="home_content text-center">
+											<div class="home_title">Un moment de reve</div>
+											<div class="booking_form_container">
+												<p class="text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe odio adipisci est atque in obcaecati reiciendis aspernatur. Dolore exercitationem maiores reprehenderit. Nam sint provident molestiae minima delectus? Optio, perferendis laboriosam.</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<!-- Home Slider Dots -->
+				<!-- <div class="home_slider_dots_container">
+				<div class="home_slider_dots">
+					<ul id="home_slider_custom_dots" class="home_slider_custom_dots d-flex flex-row align-items-start justify-content-start">
+						<li class="home_slider_custom_dot active">01.</li>
+						<li class="home_slider_custom_dot">02.</li>
+						<li class="home_slider_custom_dot">03.</li>
+					</ul>
+				</div>
+			</div> -->
+
+			</div>
+		</div>
+
+
+		<form action="#" class="booking_form">
+			<hr>
+			<h2>Recherche un appartement ou une chambre d'hotel</h2>
+			<div class="d-flex flex-xl-row flex-column align-items-start justify mt-5">
+				<div class="booking_input_container d-flex flex-lg-row flex-column align-items-start justify-content-start">
+					<div class="col-3"><input type="date" class="form-control" placeholder="Check in"  required="required"></div>
+					<div class="col-3"><input type="date" class="form-control" placeholder="Check out" required="required"></div>
+					<div class="col-3"><input type="number" class="form-control" placeholder="Children" required="required"></div>
+					<div class="col-3"><input type="number" class="form-control" placeholder="Room" required="required"></div>
+				</div>
+				<div><button class="btn btn-primary">rechercher</button></div>
+			</div>
+		</form>
+	</div>
+	<!-- Features -->
+
+	<div class="features">
+		<div class="container">
+			<div class="row">
+
+				<!-- Icon Box -->
+				<div class="col-lg-4 icon_box_col">
+					<div class="icon_box d-flex flex-column align-items-center justify-content-start text-center">
+						<div class="icon_box_icon"><img src="images/icon_1.svg" class="svg" alt="#"></div>
+						<div class="icon_box_title">
+							<h2>Etablissement fabuleux</h2>
+						</div>
+						<div class="icon_box_text">
+							<p>Que ce soit pour un séjour de courte durée ou de longue durée, il combine élégance, modernité et convivialité pour garantir une expérience inoubliable.</p>
+						</div>
+					</div>
+				</div>
+
+				<!-- Icon Box -->
+				<div class="col-lg-4 icon_box_col">
+					<div class="icon_box d-flex flex-column align-items-center justify-content-start text-center">
+						<div class="icon_box_icon"><img src="images/icon_2.svg" class="svg" alt="#"></div>
+						<div class="icon_box_title">
+							<h2>Infinity Pool</h2>
+						</div>
+						<div class="icon_box_text">
+							<p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse nec faucibus velit. Quisque eleifend orci ipsum, a bibendum.</p>
+						</div>
+					</div>
+				</div>
+
+				<!-- Icon Box -->
+				<div class="col-lg-4 icon_box_col">
+					<div class="icon_box d-flex flex-column align-items-center justify-content-start text-center">
+						<div class="icon_box_icon"><img src="images/icon_3.svg" class="svg" alt="#"></div>
+						<div class="icon_box_title">
+							<h2>Chambres de haut-niveau</h2>
+						</div>
+						<div class="icon_box_text">
+							<p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse nec faucibus velit. Quisque eleifend orci ipsum, a bibendum.</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Gallery -->
+
+	<div class="gallery">
+		<div class="gallery_slider_container">
+			<div class="owl-carousel owl-theme gallery_slider">
+
+				<!-- Slide -->
+				<div class="gallery_item">
+					<div class="background_image" style="background-image:url(images/gallery_1.jpg)"></div>
+					<a class="colorbox" href="images/gallery_1.jpg"></a>
+				</div>
+
+				<!-- Slide -->
+				<div class="gallery_item">
+					<div class="background_image" style="background-image:url(images/gallery_2.jpg)"></div>
+					<a class="colorbox" href="images/gallery_2.jpg"></a>
+				</div>
+
+				<!-- Slide -->
+				<div class="gallery_item">
+					<div class="background_image" style="background-image:url(images/gallery_3.jpg)"></div>
+					<a class="colorbox" href="images/gallery_3.jpg"></a>
+				</div>
+
+				<!-- Slide -->
+				<div class="gallery_item">
+					<div class="background_image" style="background-image:url(images/gallery_4.jpg)"></div>
+					<a class="colorbox" href="images/gallery_4.jpg"></a>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<!-- Booking -->
+
+	<div class="booking">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="booking_title text-center">
+						<h2>Reserver une chambre</h2>
+					</div>
+					<div class="booking_text text-center">
+						<p>Réservez votre chambre dès maintenant et profitez d’un séjour alliant confort, tranquillité et qualité de service.
+							Que vous voyagiez pour les affaires ou pour le plaisir, notre établissement vous propose des chambres modernes,
+							entièrement équipées, et adaptées à tous vos besoins. En quelques clics, choisissez votre type de chambre,
+							vos dates de séjour et bénéficiez des meilleurs tarifs disponibles. Notre équipe reste à votre écoute pour rendre votre expérience inoubliable.
+							Ne perdez plus de temps — votre confort vous attend</p>
+					</div>
+
+					<!-- Booking Slider -->
+					<div class="booking_slider_container">
+						<div class="owl-carousel owl-theme booking_slider">
+
+							<!-- Slide -->
+							<div class="booking_item">
+								<div class="background_image" style="background-image:url(images/booking_1.jpg)"></div>
+								<div class="booking_overlay trans_200"></div>
+								<div class="booking_price">$120/Nuit</div>
+								<div class="booking_link"><a href="booking.html">Chambre Familiale</a></div>
+							</div>
+
+							<!-- Slide -->
+							<div class="booking_item">
+								<div class="background_image" style="background-image:url(images/booking_2.jpg)"></div>
+								<div class="booking_overlay trans_200"></div>
+								<div class="booking_price">$120/Nuit</div>
+								<div class="booking_link"><a href="booking.html">Chambre de luxe</a></div>
+							</div>
+
+							<!-- Slide -->
+							<div class="booking_item">
+								<div class="background_image" style="background-image:url(images/booking_3.jpg)"></div>
+								<div class="booking_overlay trans_200"></div>
+								<div class="booking_price">$120/Nuit</div>
+								<div class="booking_link"><a href="booking.html">Chambre simple</a></div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Blog -->
+
+	<div class="blog">
+
+		<!-- Blog Slider -->
+		<div class="blog_slider_container">
+			<div class="owl-carousel owl-theme blog_slider">
+
+				<!-- Slide -->
+				<div class="blog_slide">
+					<div class="background_image" style="background-image:url(images/index_blog_1.jpg)"></div>
+					<div class="blog_content">
+						<div class="blog_date"><a href="#">Oct 20, 2018</a></div>
+						<div class="blog_title"><a href="#">How to book your stay</a></div>
+					</div>
+				</div>
+
+				<!-- Slide -->
+				<div class="blog_slide">
+					<div class="background_image" style="background-image:url(images/index_blog_2.jpg)"></div>
+					<div class="blog_content">
+						<div class="blog_date"><a href="#">Oct 20, 2018</a></div>
+						<div class="blog_title"><a href="#">10 restaurants in town</a></div>
+					</div>
+				</div>
+
+				<!-- Slide -->
+				<div class="blog_slide">
+					<div class="background_image" style="background-image:url(images/index_blog_3.jpg)"></div>
+					<div class="blog_content">
+						<div class="blog_date"><a href="#">Oct 20, 2018</a></div>
+						<div class="blog_title"><a href="#">A perfect wedding</a></div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+     <div class="container">
+               @yield('content')
+            </div>
+
+	<!-- Footer -->
+
+	@include('clients.partials.footer')
+	</div>
+
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="styles/bootstrap-4.1.2/popper.js"></script>
+	<script src="styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+	<script src="plugins/greensock/TweenMax.min.js"></script>
+	<script src="plugins/greensock/TimelineMax.min.js"></script>
+	<script src="plugins/scrollmagic/ScrollMagic.min.js"></script>
+	<script src="plugins/greensock/animation.gsap.min.js"></script>
+	<script src="plugins/greensock/ScrollToPlugin.min.js"></script>
+	<script src="plugins/OwlCarousel2-2.3.4/owl.carousel.js"></script>
+	<script src="plugins/easing/easing.js"></script>
+	<script src="plugins/progressbar/progressbar.min.js"></script>
+	<script src="plugins/parallax-js-master/parallax.min.js"></script>
+	<script src="plugins/jquery-datepicker/jquery-ui.js"></script>
+	<script src="plugins/colorbox/jquery.colorbox-min.js"></script>
+	<script src="js/custom.js"></script>
+</body>
+
+</html>
