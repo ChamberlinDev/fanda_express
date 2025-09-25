2b959c19090005e0baba3742a1e1a3ed29c49ec7
ChamberlinDev <himalayaommet@gmail.com>
2025-09-25 13:47:39 +0000

    save_25_reservation_form

--- resources/views/clients/partials/apropos.blade.php
+++ resources/views/clients/partials/apropos.blade.php
@@ -1,32 +1,28 @@
 <section class="container-fluid my-5">
-		<div class="card h-100 p-4">
-			<div class="row g-4 align-items-center">
+	<div class="card h-100 p-4">
+		<div class="row g-4 align-items-center">
 
-				<!-- Colonne texte -->
-				<div class="col-md-6">
-					<h2 class="text-center">Qui sommes-nous?</h2>
-					<p>
-						Fanda-express lor sit amet consectetur adipisicing elit.
-						Dicta sint nemo, excepturi quibusdam perspiciatis quod laborum repudiandae
-						rem veritatis incidunt totam aperiam odio ipsa, quidem praesentium ducimus
-						quaerat quia! Nisi. <br><br>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Dicta sint nemo, excepturi quibusdam perspiciatis quod laborum repudiandae
-						rem veritatis incidunt totam aperiam odio ipsa, quidem praesentium ducimus
-						quaerat quia! Nisi. <br><br>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Dicta sint nemo, excepturi quibusdam perspiciatis quod laborum repudiandae
-						rem veritatis incidunt totam aperiam odio ipsa, quidem praesentium ducimus
-						quaerat quia! Nisi.
-					</p>
-				</div>
+			<!-- Colonne texte -->
+			<div class="col-md-6">
+				<h2 class="text-center">Qui sommes-nous?</h2>
+				<p>
+					Fanda-express est une plateforme en ligne de réservation d’hôtels qui connecte les voyageurs avec une variété d’établissements.
+					Les clients peuvent facilement rechercher, comparer et réserver des chambres adaptées à leurs besoins, tandis que
+					les hôteliers peuvent gérer leurs établissements de manière autonome.
 
-				<!-- Colonne image -->
-				<div class="col-md-6 text-center">
-					<img src="../images/working-4.jpg" alt="Image illustrative"
-						class="img-fluid rounded">
-				</div>
-
+					Lancée avec un budget minimal,
+					la plateforme se concentre sur la simplicité et l’efficacité, en proposant
+					 des options de paiement accessibles comme MTN MOMO. Elle vise à moderniser 
+					 le secteur hôtelier local en offrant une solution pratique et inclusive.
+				</p>
 			</div>
+
+			<!-- Colonne image -->
+			<div class="col-md-6 text-center">
+				<img src="../images/working-4.jpg" alt="Image illustrative"
+					class="img-fluid rounded">
+			</div>
+
 		</div>
-	</section>
+	</div>
+</section>
