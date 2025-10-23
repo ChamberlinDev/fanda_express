f7dd914b2e88759f48925ca2210235de0ad1f259
ChamberlinDev <himalayaommet@gmail.com>
2025-10-23 23:15:27 +0000

    save_template23

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -7,15 +7,13 @@
     <title>Fanda-express - Admin Dashboard</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-    <link rel="icon" href="/admin/assets/img/kaiadmin/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="{{ url('admin/assets/img/kaiadmin/favicon.ico') }}" type="image/x-icon" />
 
     <!-- Fonts and icons -->
-    <script src="/admin/assets/js/plugin/webfont/webfont.min.js"></script>
+    <script src="{{ url('admin/assets/js/plugin/webfont/webfont.min.js') }}"></script>
     <script>
         WebFont.load({
-            google: {
-                families: ["Public Sans:300,400,500,600,700"]
-            },
+            google: { families: ["Public Sans:300,400,500,600,700"] },
             custom: {
                 families: [
                     "Font Awesome 5 Solid",
@@ -23,18 +21,18 @@
                     "Font Awesome 5 Brands",
                     "simple-line-icons"
                 ],
-                urls: ["/admin/assets/css/fonts.min.css"]
+                urls: ["{{ url('admin/assets/css/fonts.min.css') }}"]
             },
-            active: function() {
+            active: function () {
                 sessionStorage.fonts = true;
             }
         });
     </script>
 
     <!-- CSS Files -->
-    <link rel="stylesheet" href="/admin/assets/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/admin/assets/css/plugins.min.css" />
-    <link rel="stylesheet" href="/admin/assets/css/kaiadmin.min.css" />
+    <link rel="stylesheet" href="{{ url('admin/assets/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ url('admin/assets/css/plugins.min.css') }}" />
+    <link rel="stylesheet" href="{{ url('admin/assets/css/kaiadmin.min.css') }}" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" />
 
     <!-- Optional demo CSS (à retirer en prod si inutile) -->
@@ -69,37 +67,37 @@
     </div>
 
     <!--   Core JS Files   -->
-    <script src="/admin/assets/js/core/jquery-3.7.1.min.js"></script>
-    <script src="/admin/assets/js/core/popper.min.js"></script>
-    <script src="/admin/assets/js/core/bootstrap.min.js"></script>
+    <script src="{{ url('admin/assets/js/core/jquery-3.7.1.min.js') }}"></script>
+    <script src="{{ url('admin/assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ url('admin/assets/js/core/bootstrap.min.js') }}"></script>
 
     <!-- jQuery Scrollbar -->
-    <script src="/admin/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+    <script src="{{ url('admin/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js') }}"></script>
 
     <!-- Chart JS -->
-    <script src="/admin/assets/js/plugin/chart.js/chart.min.js"></script>
+    <script src="{{ url('admin/assets/js/plugin/chart.js/chart.min.js') }}"></script>
 
     <!-- jQuery Sparkline -->
-    <script src="/admin/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js"></script>
+    <script src="{{ url('admin/assets/js/plugin/jquery.sparkline/jquery.sparkline.min.js') }}"></script>
 
     <!-- Chart Circle -->
-    <script src="/admin/assets/js/plugin/chart-circle/circles.min.js"></script>
+    <script src="{{ url('admin/assets/js/plugin/chart-circle/circles.min.js') }}"></script>
 
     <!-- Datatables -->
-    <script src="/admin/assets/js/plugin/datatables/datatables.min.js"></script>
+    <script src="{{ url('admin/assets/js/plugin/datatables/datatables.min.js') }}"></script>
 
     <!-- Bootstrap Notify -->
-    <script src="/admin/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js"></script>
+    <script src="{{ url('admin/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}"></script>
 
     <!-- jQuery Vector Maps -->
-    <script src="/admin/assets/js/plugin/jsvectormap/jsvectormap.min.js"></script>
-    <script src="/admin/assets/js/plugin/jsvectormap/world.js"></script>
+    <script src="{{ url('admin/assets/js/plugin/jsvectormap/jsvectormap.min.js') }}"></script>
+    <script src="{{ url('admin/assets/js/plugin/jsvectormap/world.js') }}"></script>
 
     <!-- Sweet Alert -->
-    <script src="/admin/assets/js/plugin/sweetalert/sweetalert.min.js"></script>
+    <script src="{{ url('admin/assets/js/plugin/sweetalert/sweetalert.min.js') }}"></script>
 
     <!-- Kaiadmin JS -->
-    <script src="/admin/assets/js/kaiadmin.min.js"></script>
+    <script src="{{ url('admin/assets/js/kaiadmin.min.js') }}"></script>
 
     <!-- Scripts spécifiques aux pages -->
     @yield('scripts')
@@ -116,5 +114,4 @@
         });
     </script>
 </body>
-
 </html>
