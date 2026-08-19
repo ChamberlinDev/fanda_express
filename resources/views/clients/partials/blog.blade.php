1027c5b58d368890f1a7e35875beaaf5925ab40b
ChamberlinDev <himalayaommet@gmail.com>
2026-08-19 21:49:09 +0000

    details_blog

--- resources/views/clients/partials/blog.blade.php
+++ resources/views/clients/partials/blog.blade.php
@@ -68,7 +68,7 @@
     @else
         <div class="text-center py-5 text-muted">
             <i class="bi bi-newspaper fs-1 d-block mb-3"></i>
-            Aucun article disponible pour le moment.
+            Aucun blog disponible pour le moment.
         </div>
     @endif
 
