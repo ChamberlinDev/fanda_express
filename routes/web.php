8cb410679bb09e51a0694fdd3789adee668281bb
root <root@vmi3420775.contaboserver.net>
2026-08-19 23:55:33 +0200

    modifications locales blog

--- routes/web.php
+++ routes/web.php
@@ -28,31 +28,18 @@ Route::get('/hotels', [HotelController::class, 'search_hotel']);
 Route::get('/recherche', [RechercheController::class, 'rechercher'])->name('recherche.resultats');
 
 
-// reservation hotel
+// reservation hotel (côté client)
 
 Route::get('/reservation_hotel/{id}', [ReservationController::class, 'create'])->name('reservation_hotel');
-Route::get('/reservations/{id}/accepter', [ReservationController::class, 'accepter'])->name('reservations_accepter');
-Route::get('/reservations/{id}/refuser', [ReservationController::class, 'refuser'])->name('reservations_refuser');
-Route::put('/reservations/{id}/update-statut', [ReservationController::class, 'updateStatut'])->name('reservations_update_statut');
+Route::post('/reservation', [ReservationController::class, 'store'])->name('reservations.store');
 
-
-
-
-
-Route::get('/reservation_admin', [ReservationController::class, 'reservation_admin']);
-// reservation appart
+// reservation appart (côté client)
 Route::get('/reservation_etablissements/{id}', [ReservationController::class, 'create_appart'])->name('reservation_appart');
 Route::post('/reservation_appart', [ReservationController::class, 'store_appart'])->name('reservations_appart.store');
-Route::get('/reservations_appart/{id}/accepter', [ReservationController::class, 'accepter_appart'])->name('reservations_appart_accepter');
-Route::get('/reservations_appart/{id}/refuser', [ReservationController::class, 'refuser_appart'])->name('reservations_appart_refuser');
-Route::put('/reservations_appart/{id}/update-statut', [ReservationController::class, 'updateStatut_appart'])->name('reservations_appart_update_statut');
-
-Route::post('/reservation', [ReservationController::class, 'store'])->name('reservations.store');
-Route::get('/hotels/{id}/reservations', [HotelController::class, 'reservations'])->name('hotels.reservations');
 
 
 // routes de connexion
-Route::get('/connexion', [Authcontroller::class, 'loginform']);
+Route::get('/connexion', [Authcontroller::class, 'loginform'])->name('login');
 Route::post('/register', [Authcontroller::class, 'register']);
 Route::post('/login', [Authcontroller::class, 'login']);
 Route::get('/change_password', [Authcontroller::class, 'change_password'])->name('change_password');
@@ -60,75 +47,90 @@ Route::post('/change-password', [AuthController::class, 'changePassword'])->name
 
 Route::get('/deconnexion', [Authcontroller::class, 'logout'])->name('logout');
 
-// Routes gerants
-Route::get('/home', [Authcontroller::class, 'home']);
-
-// Route pour gerer le profil
-Route::get('/profil', [Authcontroller::class, 'profil'])->name('admin.profil');
-Route::get('/profil_edit', [Authcontroller::class, 'edit_profil']);
-Route::post('/profil_save', [Authcontroller::class, 'update'])->name('profil_save');
-
-
-// clients partie/admin
-Route::get('/clients', [Authcontroller::class, 'clients_liste'])->name('admin.clients');
-
-
-// Route pour gerer les finances
-Route::get('/caisse', [FinanceController::class, 'index'])->name('admin.caisse');
-Route::get('/rapport', [FinanceController::class, 'rapport_index'])->name('admin.rapport');
-Route::get('/rapport/create', [FinanceController::class, 'create_rapport'])->name('admin.rapport.create');
-Route::post('/rapport/store', [FinanceController::class, 'store_rapport'])->name('admin.rapport.store');
-Route::delete('/admin/rapports/{id}', [FinanceController::class, 'destroy'])->name('admin.rapport.destroy');
-
-
-// Route pour gerer les hotels
-Route::get('/etablissement', [HotelController::class, 'index'])->name('admin.etablissements');
-Route::get('/ajouter_eta', [HotelController::class, 'Ajouter_hotel']);
-Route::post('/create_hotel', [HotelController::class, 'create'])->name('create_hotel');
-Route::delete('/hotels_delete/{id}', [HotelController::class, 'destroy'])->name('etablissements.destroy');
-Route::get('/show_hotel/{id}', [HotelController::class, 'show'])->name('etablissements.show');
-
+// fiches détail publiques (consultées par les clients avant réservation)
 Route::get('/details/{id}', [HotelController::class, 'details'])->name('hotel.show');
-Route::delete('/supp_hotel/{id}', [HotelController::class, 'destroy'])->name('supp_hotel');
-
-Route::get('/modif_form/{id}', [HotelController::class, 'edit']);
-Route::post('/modif_save/{id}', [HotelController::class, 'update'])->name('modif_save');
-
-// Route pour les appartements
-Route::get('/ajouter_appart', [AppartementController::class, 'Ajouter_appart']);
-Route::post('/create', [AppartementController::class, 'create'])->name('create_appart');
-Route::get('/show_appart/{id}', [AppartementController::class, 'show'])->name('show_appart');
 Route::get('/details_appart/{id}', [AppartementController::class, 'details'])->name('appart.show');
-Route::get('/modif_edit/{id}', [AppartementController::class, 'edit']);
-Route::post('/modif_appart/{id}', [AppartementController::class, 'update'])->name('modif_appart');
-Route::delete('/supp_appart/{id}', [AppartementController::class, 'destroy'])->name('supp_appart');
 
-// Route pour chambre
-Route::get('/chambres/{id}', [ChambreController::class, 'create'])->name('chambres.create');
-Route::post('/etablissements/{id}/chambres', [ChambreController::class, 'store'])->name('chambres.store');
-Route::get('/chambres/{id}/edit',    [ChambreController::class, 'edit'])->name('chambres.edit');
-Route::put('/chambres/{id}',         [ChambreController::class, 'update'])->name('chambres.update');
-Route::delete('/chambres/{id}',      [ChambreController::class, 'destroy'])->name('chambres.destroy');
-
-// Route pour gerer les reservations
-Route::get('/reservation', [ReservationController::class, 'index'])->name('reservation.index');
-
-// Route pour gerer les blogs
-Route::get('/blog', [blogcontroller::class, 'index'])->name('admin.blog');
-Route::get('/ajouter_blog', [blogcontroller::class, 'ajout_form']);
-Route::get('/show_blog/{id}', [blogcontroller::class, 'show'])->name('blog.show');
-Route::get('/edit_blog/{id}', [blogcontroller::class, 'edit'])->name('blog.edit');
-Route::post('/update_blog/{id}', [blogcontroller::class, 'update'])->name('blog.update');
-Route::delete('/delete_blog/{id}', [blogcontroller::class, 'destroy'])->name('blog.destroy');
-Route::post('/ajout_save', [blogcontroller::class, 'store']);
-
-// commentaires
-
+// commentaires (soumission publique)
 Route::post('/commentaire_app', [CommentaireController::class, 'store_app'])->name('commentaire_app');
 
+// contact (soumission publique)
+Route::post('/contact', [ConctactController::class, 'send'])->name('contact.send');
 
 
+// ---------------------------------------------------------------------
+// Routes de gestion (gérants d'hôtels/appartements) - authentification requise
+// ---------------------------------------------------------------------
+Route::middleware(['auth'])->group(function () {
 
+    // reservations (gestion)
+    Route::get('/reservation_admin', [ReservationController::class, 'reservation_admin']);
+    Route::get('/reservations/{id}/accepter', [ReservationController::class, 'accepter'])->name('reservations_accepter');
+    Route::get('/reservations/{id}/refuser', [ReservationController::class, 'refuser'])->name('reservations_refuser');
+    Route::put('/reservations/{id}/update-statut', [ReservationController::class, 'updateStatut'])->name('reservations_update_statut');
+    Route::get('/reservations_appart/{id}/accepter', [ReservationController::class, 'accepter_appart'])->name('reservations_appart_accepter');
+    Route::get('/reservations_appart/{id}/refuser', [ReservationController::class, 'refuser_appart'])->name('reservations_appart_refuser');
+    Route::put('/reservations_appart/{id}/update-statut', [ReservationController::class, 'updateStatut_appart'])->name('reservations_appart_update_statut');
+    Route::get('/hotels/{id}/reservations', [HotelController::class, 'reservations'])->name('hotels.reservations');
+    Route::get('/reservation', [ReservationController::class, 'index'])->name('reservation.index');
+
+    // Routes gerants
+    Route::get('/home', [Authcontroller::class, 'home']);
+
+    // Route pour gerer le profil
+    Route::get('/profil', [Authcontroller::class, 'profil'])->name('admin.profil');
+    Route::get('/profil_edit', [Authcontroller::class, 'edit_profil']);
+    Route::post('/profil_save', [Authcontroller::class, 'update'])->name('profil_save');
+
+    // clients partie/admin
+    Route::get('/clients', [Authcontroller::class, 'clients_liste'])->name('admin.clients');
+
+    // Route pour gerer les finances
+    Route::get('/caisse', [FinanceController::class, 'index'])->name('admin.caisse');
+    Route::get('/rapport', [FinanceController::class, 'rapport_index'])->name('admin.rapport');
+    Route::get('/rapport/create', [FinanceController::class, 'create_rapport'])->name('admin.rapport.create');
+    Route::post('/rapport/store', [FinanceController::class, 'store_rapport'])->name('admin.rapport.store');
+    Route::delete('/admin/rapports/{id}', [FinanceController::class, 'destroy'])->name('admin.rapport.destroy');
+
+    // Route pour gerer les hotels
+    Route::get('/etablissement', [HotelController::class, 'index'])->name('admin.etablissements');
+    Route::get('/ajouter_eta', [HotelController::class, 'Ajouter_hotel']);
+    Route::post('/create_hotel', [HotelController::class, 'create'])->name('create_hotel');
+    Route::delete('/hotels_delete/{id}', [HotelController::class, 'destroy'])->name('etablissements.destroy');
+    Route::get('/show_hotel/{id}', [HotelController::class, 'show'])->name('etablissements.show');
+    Route::delete('/supp_hotel/{id}', [HotelController::class, 'destroy'])->name('supp_hotel');
+    Route::get('/modif_form/{id}', [HotelController::class, 'edit']);
+    Route::post('/modif_save/{id}', [HotelController::class, 'update'])->name('modif_save');
+
+    // Route pour les appartements
+    Route::get('/ajouter_appart', [AppartementController::class, 'Ajouter_appart']);
+    Route::post('/create', [AppartementController::class, 'create'])->name('create_appart');
+    Route::get('/show_appart/{id}', [AppartementController::class, 'show'])->name('show_appart');
+    Route::get('/modif_edit/{id}', [AppartementController::class, 'edit']);
+    Route::post('/modif_appart/{id}', [AppartementController::class, 'update'])->name('modif_appart');
+    Route::delete('/supp_appart/{id}', [AppartementController::class, 'destroy'])->name('supp_appart');
+
+    // Route pour chambre
+    Route::get('/chambres/{id}', [ChambreController::class, 'create'])->name('chambres.create');
+    Route::post('/etablissements/{id}/chambres', [ChambreController::class, 'store'])->name('chambres.store');
+    Route::get('/chambres/{id}/edit', [ChambreController::class, 'edit'])->name('chambres.edit');
+    Route::put('/chambres/{id}', [ChambreController::class, 'update'])->name('chambres.update');
+    Route::delete('/chambres/{id}', [ChambreController::class, 'destroy'])->name('chambres.destroy');
+
+    // Route pour gerer les blogs
+    Route::get('/blog', [blogcontroller::class, 'index'])->name('admin.blog');
+    Route::get('/ajouter_blog', [blogcontroller::class, 'ajout_form']);
+    Route::get('/show_blog/{id}', [blogcontroller::class, 'show'])->name('blog.show');
+    Route::get('/edit_blog/{id}', [blogcontroller::class, 'edit'])->name('blog.edit');
+    Route::post('/update_blog/{id}', [blogcontroller::class, 'update'])->name('blog.update');
+    Route::delete('/delete_blog/{id}', [blogcontroller::class, 'destroy'])->name('blog.destroy');
+    Route::post('/ajout_save', [blogcontroller::class, 'store']);
+});
+
+
+// ---------------------------------------------------------------------
+// Routes super-admin - authentification + rôle admin requis
+// ---------------------------------------------------------------------
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'admin_view'])->name('superadmin.dashboard');
     Route::get('/inscription', [Authcontroller::class, 'registerform']);
@@ -147,17 +149,15 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/blog_sup', [BlogController::class, 'liste_blog'])->name('superadmin.blogs');
     Route::get('/blog_sup/{id}/edit', [BlogController::class, 'modif_view'])->name('superadmin.blogs.edit');
     Route::post('/blog_sup/{id}/update', [BlogController::class, 'update_blog'])->name('superadmin.blogs.update');
-    Route::delete('/blog_sup/{id}', [BlogController::class, 'supprimer'])->name('superadmin.blogs.destroy'); 
+    Route::delete('/blog_sup/{id}', [BlogController::class, 'supprimer'])->name('superadmin.blogs.destroy');
     Route::get('/blog_sup/create', [BlogController::class, 'ajouter'])->name('superadmin.blogs.create');
     Route::post('/blog_sup/store', [BlogController::class, 'create'])->name('superadmin.blogs.store');
-    Route::get('/blog_sup/{id}', [BlogController::class, 'details'])->name('superadmin.blogs.show');  
+    Route::get('/blog_sup/{id}', [BlogController::class, 'details'])->name('superadmin.blogs.show');
 
-    Route::get('/commentaires', [CommentaireController::class, 'liste_commentaires'])->name('superadmin.commentaires');  
+    Route::get('/commentaires', [CommentaireController::class, 'liste_commentaires'])->name('superadmin.commentaires');
 
 
     Route::get('/profil/admin', [Authcontroller::class, 'profil_admin'])->name('superadmin.profil');
     Route::get('/profil_edit/admin', [Authcontroller::class, 'edit_profil_admin'])->name('superadmin.profil_edit');
     Route::post('/profil_save/admin', [Authcontroller::class, 'update_admin'])->name('superadmin.profil_save');
 });
-
-Route::post('/contact', [ConctactController::class, 'send'])->name('contact.send');
